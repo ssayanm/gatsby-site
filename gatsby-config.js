cc7b100f93ce9a8f486c43897219e005d7d00e2d
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-02-13 17:36:40 +0530

    adding blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,15 @@ module.exports = {
     title: `Full-Stack Developer!`,
     author: `Sayan Mukherjee`,
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,35 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date
+            }
+            excerpt
+          }
+        }
+        totalCount
+      }
+    }
+  `)
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up below</p>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          {node.frontmatter.title} - {node.frontmatter.date}
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </Layout>
   )
 }
