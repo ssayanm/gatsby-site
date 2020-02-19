2d6fa8b508e0fd0235edfc1f366ff28b1ec1b47d
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-02-19 11:57:20 +0530

    added styling on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,8 @@ import React from "react"
 import Layout from "../components/layout"
 import { Link, graphql, useStaticQuery } from "gatsby"
 
+import blogStyles from "./blog.module.scss"
+
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -27,10 +29,10 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <li>
+            <li className={blogStyles.post}>
               <Link to={`/blog/${node.fields.slug}`}>
                 <h2>{node.frontmatter.title}</h2>
               </Link>
