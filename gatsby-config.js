462ccbea3f1fb0dac27d5f920c9f0fc6b6280dba
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-03-17 15:47:18 +0530

    removed slider

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     author: `Sayan Mukherjee`,
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
