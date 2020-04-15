9f5a158d74f3d3f88e44a268456d43bd7ab49966
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-04-15 06:30:20 +0530

    fixed two warnings

--- src/components/faq.js
+++ src/components/faq.js
@@ -6,6 +6,9 @@ function FAQ({ faq, index, toggleFAQ }) {
       className={"faq " + (faq.open ? "open" : "")}
       key={index}
       onClick={() => toggleFAQ(index)}
+      onKeyDown={() => toggleFAQ(index)}
+      role="button"
+      tabIndex="0"
     >
       <div className="faq-question">{faq.question}</div>
       <div className="faq-answer">{faq.answer}</div>
